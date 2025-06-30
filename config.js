9720dfd4d54444241c8f07b69eaf862c624922eb
jrio2007 <josimioni@gmail.com>
2025-06-30 10:32:35 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-const NGROK_URL = "https://2fbb-179-125-133-145.ngrok-free.app"; // ← troque aqui quando o ngrok mudar
+const NGROK_URL = "https://2fbb-179-125-133-145.ngrok-free.app/imprimir"; // ← troque aqui quando o ngrok mudar
